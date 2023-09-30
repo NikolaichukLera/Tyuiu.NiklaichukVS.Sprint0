125acb1d3dfeb63ec06501d2e4663e75f3652b5e
Tyuiu.NikolaichukVS <nikolaichuk.lera@mail.ru>
2023-09-30 18:21:51 +0500

    Task1

--- Tyuiu.NikolaichukVS.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.NikolaichukVS.Sprint0.Task1.V0/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tyuiu.NikolaichukVS.Sprint0.Task0.V0
+namespace Tyuiu.NikolaichukVS.Sprint0.Task1.V0
 {
     class Program
     {
@@ -13,7 +13,7 @@ namespace Tyuiu.NikolaichukVS.Sprint0.Task0.V0
             Console.WriteLine("Hello, World");
             string name = "Валерия";
             int age = 17;
-            Console.WriteLine("Здравствуйте меня зовут " + name + " мне" + age + " лет!");
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
     }
